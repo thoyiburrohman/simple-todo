7db348d8373b4ca1fc985dea86f061c269c8d287
thoyiburrohman <thoyibur@gmail.com>
2025-06-15 11:40:05 +0700

    update route console

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Console\Commands\DailyTaskReminder;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command(DailyTaskReminder::class);
